15cdd5904a43f752148af567da3c3703de75250c
jkpark <pjk0007@naver.com>
2025-08-27 13:04:10 +0900

    feat: object stroage 다운로드,삭제 기능

--- src/app/example/object_storage/page.tsx
+++ src/app/example/object_storage/page.tsx
@@ -1,35 +1,33 @@
+import ImageCard from '@/app/example/object_storage/components/ImageCard';
 import Uploader from '@/app/example/object_storage/components/Uploader';
-import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card';
-import { getPresignedUrl, listObjects } from '@/lib/storage';
+import { downloadObject, getPresignedUrl, listObjects } from '@/lib/storage';
+import { revalidatePath } from 'next/cache';
 
 export default async function ObjectStoragePage() {
-    const data = await listObjects({});
+    let data = await listObjects({});
+
+    async function reloadData() {
+        'use server';
+        revalidatePath('/example/object_storage'); // 현재 경로
+    }
 
     return (
         <div className="flex flex-col gap-4 p-4">
-            <Uploader objectPath={`test/`} />
+            <Uploader
+                objectPath={`test/`}
+                reloadData={reloadData}
+            />
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 {data.Contents?.filter(item => item.Key).map(async item => {
                     const url = await getPresignedUrl(item.Key!);
+
                     return (
-                        <Card>
-                            <CardHeader>{item.Key}</CardHeader>
-                            <CardContent>
-                                <img
-                                    src={url}
-                                    alt={item.Key!}
-                                />
-                            </CardContent>
-                            <CardFooter>
-                                <a
-                                    href={url}
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                >
-                                    View Image
-                                </a>
-                            </CardFooter>
-                        </Card>
+                        <ImageCard 
+                            key={item.Key}
+                            objectKey={item.Key!}
+                            url={url}
+                            reloadData={reloadData}
+                        />
                     );
                 })}
             </div>
